24e585720a6d51a74919ebe1f883d35454792b83
mmcguire91 <mmcguire91@gmail.com>
2020-06-10 17:19:47 -0500

    Established API formatting, used dummy data to retrieve info from API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
+//requires user to specify package.DataType
+import 'package:convert/convert.dart';
+//imports jsonDecode
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -25,6 +30,38 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  //API Call
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    /*http 200 return request = successful call
+     If there was a successful API call
+     store the longitude data retrieved in JSON format from API call in var longitude
+     print the longitude data to the console*/
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      //body property of Response holds response as a string
+      var longitude = jsonDecode(data)['coord']['lon'];
+      //retrieve longitude data from API call in JSON format and store in var longitude
+      print(longitude);
+
+      var decodedData = jsonDecode(data);
+      //required for us to specify data types
+      double temperature = decodedData['main']['temp'];
+      print(temperature);
+
+      int condition = decodedData['weather'][0]['id'];
+      print(condition);
+
+      String city = decodedData['name'];
+      print(city);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void deactivate() {
     super.deactivate();
@@ -32,6 +69,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
